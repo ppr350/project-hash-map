d999837e4d23bfc63b51283fb5668cc2d5575d3a
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2024-05-23 22:01:53 +0100

    added slef limit function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Project Hash Map')
+
+// self limit function
+function limitArrayLenght() {
+    if (index < 0 || index >= buckets.length) {
+        throw new Error('Trying to access index out of bound')
+    }
+}
+
